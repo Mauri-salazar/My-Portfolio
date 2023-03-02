767195866a4e3262af891811ec68b9ac04bcc0eb
Mauri-salazar <maurisa2004@gmail.com>
2023-03-02 15:31:56 -0300

    wapper

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Wrapper from './componetes/Wapper/Wapper ';
+import Wrapper from './componetes/Wapper/Wapper';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
