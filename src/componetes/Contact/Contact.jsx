4a3451905541a5d90e999432c052e7616894c73f
Mauri-salazar <salazar.mauricio.dev@gmail.com>
2023-09-23 01:44:35 -0300

    Editing form, adding full name and contact reason fields and linking a new email replace maurisa2004@gmail.com with salazar.mauricio.dev@gmail.com

--- src/componetes/Contact/Contact.jsx
+++ src/componetes/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Container , Row , Col } from 'react-bootstrap';
 import TrackVisibility from 'react-on-screen';
 import contactImg from '../../assets/img/contact-img.svg';
@@ -9,17 +9,19 @@ import 'animate.css';
 import { FormattedMessage } from 'react-intl';
 
 export const Contact = () => {
-
+  
   const [buttonText, setButtonText] = useState('Send');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setButtonText('Sending..');
+    setButtonText('Pending..'); 
 
-    emailJs.sendForm('service_yno5xlh','template_m2uxd6j',e.target,'udLtDZ_TCph6lDgpr')
+    emailJs.sendForm('service_3kfp76r','template_gf8ir8b',e.target,'udLtDZ_TCph6lDgpr')
     .then( response => console.log(response) )
-    .catch(error => console.log(error))
+    .catch(error => console.log(error))    
     setButtonText('Send');
+    e.target.reset();
+
   };
 
   return (
@@ -43,36 +45,11 @@ export const Contact = () => {
                   <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                     <h2><FormattedMessage id='app.contact.title' /></h2>
                     <form onSubmit={handleSubmit}>
-                      <Row>
                       <Col size={12} sm={6} className="px-1">
                         <input
                           type="text"
-                          placeholder="Firts name"
+                          placeholder="First name"
                           name='user_name'
-                          autoComplete='off'
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="text"
-                          placeholder="Last name"
-                          name='user_last_name'
-                          autoComplete='off'
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="email"
-                          placeholder="Email"
-                          name='user_email'
-                          autoComplete='off'
-                        />
-                      </Col>
-                      <Col size={12} sm={6} className="px-1">
-                        <input
-                          type="tel"
-                          placeholder="Phone"
-                          name='user_phone'
                           autoComplete='off'
                         />
                       </Col>
@@ -80,12 +57,11 @@ export const Contact = () => {
                         <textarea
                           rows="6"
                           placeholder="Account reason contact"
-                          name='user_message'
+                          name='message'
                         >
                         </textarea>
                         <button type="submit"><span>{buttonText}</span></button>
                       </Col>
-                    </Row>
                     </form>
                   </div>
               }
