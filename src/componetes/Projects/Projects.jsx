c83a284bfb845f12ff443de8bf320f3dfd709d58
Mauri-salazar <salazar.mauricio.dev@gmail.com>
2023-09-23 12:59:13 -0300

    Construction of the projects component.

--- src/componetes/Projects/Projects.jsx
+++ src/componetes/Projects/Projects.jsx
@@ -13,36 +13,17 @@ export const Projects = () => {
   return (
     <section  className='projects' id='projects'>
       <Container>
+        <div>
+          <h2><FormattedMessage id='app.projects.title'/></h2>
+          <p><FormattedMessage  id='app.projects.info'/></p>
+          
+        </div>
         <Row>
-          <Col size={12}>
-            <TrackVisibility>
-              { ({isVisible}) =>
-                  <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                    <h2><FormattedMessage id='app.projects.title'/></h2>
-                    <p><FormattedMessage  id='app.projects.info'/></p>
-                    <Tab.Container id='projects-tabs' defaultActiveKey='first'>
-                      <Nav variant='pills' className='className="nav-pills mb-5 justify-content-center align-items-center"' id="pills-tab">
-                        <Nav.Item>
-                          <Nav.Link eventKey='first'>tab 1</Nav.Link>
-                        </Nav.Item>
-                      </Nav>
-                      <Tab.Content>
-                        <Tab.Pane eventKey='first'>
-                          <Row>
-
-                          </Row>
-                        </Tab.Pane>
-                        <Tab.Pane eventKey='second'>
-                            tab second
-                        </Tab.Pane>
-                        <Tab.Pane eventKey='third'>
-                            tab third
-                        </Tab.Pane>
-                      </Tab.Content>
-                    </Tab.Container>
-                  </div>
-              }
-            </TrackVisibility>
+          <Col>
+            <div>
+              <img src='' alt=''/>
+              <h2>Title first project</h2>
+            </div>
           </Col>
         </Row>
       </Container>
